6af9a3685beb1a7565414022b2d0602d039a6eff
iOSayedAhmed <60572628+iOSayedAhmed@users.noreply.github.com>
2023-06-14 11:44:17 +0300

    Add VerifyAccountImagePickerVC

--- Monasba/ViewControllers/Categories/AskImagePickerViewController.swift
+++ Monasba/ViewControllers/Categories/AskImagePickerViewController.swift
@@ -41,15 +41,7 @@ class AskImagePickerViewController: UIViewController {
         self.dismiss(animated: false, completion: nil)
         
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
 extension AskImagePickerViewController : UIImagePickerControllerDelegate,
