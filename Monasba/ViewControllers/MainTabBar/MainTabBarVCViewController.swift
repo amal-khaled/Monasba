df7ce1ab7ee89b5dfff5a01edce10ed11891c193
aelgalant <aelgalant@badee.com.sa>
2023-08-02 15:43:55 +0300

    tabbar

--- Monasba/ViewControllers/MainTabBar/MainTabBarVCViewController.swift
+++ Monasba/ViewControllers/MainTabBar/MainTabBarVCViewController.swift
@@ -14,29 +14,5 @@ class MainTabBarVCViewController: UITabBarController{
 //        self.delegate = self
     }
   
-//    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
-//        if let index = tabBarController.viewControllers?.firstIndex(of: viewController) {
-//           
-//           if index == 3 {
-//               let vc = UIStoryboard(name: SEARCH_STORYBOARD, bundle: nil).instantiateViewController(withIdentifier: "search_base") as! SearchBaseViewController
-//
-//                          // Check if the selected view controller is already embedded in a UINavigationController.
-//                          if let navigationController = tabBarController.selectedViewController as? UINavigationController {
-//                              navigationController.pushViewController(vc, animated: true)
-//                          } else {
-//                              // If not, create a new navigation controller and set it as the selected tab's view controller.
-//                              let newNavigationController = UINavigationController(rootViewController: vc)
-//                              tabBarController.selectedViewController = newNavigationController
-//        
-//                          }
-//               
-//               return false
-//           }
-//       }
-//
-//        return true
-//    }
-//    
-//    
 
 }
